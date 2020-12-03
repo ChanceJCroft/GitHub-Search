e0a9a4cac4a897d4d9e710f1aa77f00418f607cf
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2020-12-02 19:16:28 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,18 +11,36 @@ class App extends Component {
     loading: false
   }
 
-
+//uncomment this section below if you want to have it default to showing the first X users
   /* anything in componentDidMount will fire right when the app turns on */
-  async componentDidMount() {
+  /* async componentDidMount() {
     //change loading to 'true' while awaiting axios response
     this.setState({ loading: true });
 
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUBT_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUBT_CLIENT_SECRET}`);
     //once you have the axios response, set the users to that data and loading to false
     this.setState({ users: res.data, loading: false });
+  } */
+  
+   //search github users
+  searchUsers = async text => {
+
+    //dont forget the SEARCH/USERS - messed me up. Not just /users when calling for the search. See Github API information
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUBT_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUBT_CLIENT_SECRET}`);
+    //once you have the axios response, set the users to that data and loading to false
+    //since we are searching for many users through axios, res.data wont work. We need the items from the res.data
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  clearUsers = () => {
+    //change the state to be empty when cleared
+    this.setState({ users: [], loading: false });
   }
 
   render() {
+      const { users, loading } = this.state;
+
+
       return (
       <div>
       <nav className="navbar bg-primary">
@@ -30,10 +48,12 @@ class App extends Component {
         <Navbar className="navbar bg-primary"/>
       </nav>
       <div className="container">
-        <Search />
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
+        //if there are any users being shown, showClear is true, otherwise it is false
+        showClear={users.length > 0 ? true : false }/>
         {/* Place the users below in a separate container div - CSS applied to container */}
         {/* pass in the loading and users as props through state */}
-        <Users loading={this.state.loading} users={this.state.users} />
+        <Users loading={loading} users={users} />
         </div>  
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
     onChange = (e) => {
@@ -12,15 +19,30 @@ export class Search extends Component {
     //should use arrow functions, it is cleaner and simpler when using "this"
     onSubmit = (e) => {
         e.preventDefault();
+        //passing in the TEXT of the search bar up through PROPS. searchUsers defined in App.js
+        this.props.searchUsers(this.state.text);
+        
+        //changes the text to be blank after submission 
+        this.setState({ text: '' });
     }
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
+                {/* The form calls the onSubmit function, and the onSubmit function passess in the searchUsers function */}
                 <form onSubmit={this.onSubmit} className="form">
                     <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange} />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {/* if this.props.showClear is true, then show the button */}
+                {showClear && 
+                    <button 
+                    className="btn btn-light btn-block" 
+                    onClick={clearUsers}>
+                        Clear
+                    </button>}
             </div>
         )
     }
