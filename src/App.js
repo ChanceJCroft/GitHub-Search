f18158b7104892b3bcff88b27ae5b712bdc5bb52
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2020-12-07 00:16:06 -0600

    Add files via upload
    Added Link react-dom components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -18,7 +20,6 @@ class App extends Component {
   /* async componentDidMount() {
     //change loading to 'true' while awaiting axios response
     this.setState({ loading: true });
-
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUBT_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUBT_CLIENT_SECRET}`);
     //once you have the axios response, set the users to that data and loading to false
     this.setState({ users: res.data, loading: false });
@@ -49,24 +50,33 @@ class App extends Component {
 
 
       return (
+      <Router>
       <div>
       <nav className="navbar bg-primary">
         {/* Run the NavBar component in a NavBar div */}
         <Navbar className="navbar bg-primary"/>
       </nav>
       <div className="container">
-        <Alert alert={this.state.alert} />
-        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
-        //if there are any users being shown, showClear is true, otherwise it is false
-        showClear={users.length > 0 ? true : false }
-        setAlert={this.setAlert}/>
         {/* Place the users below in a separate container div - CSS applied to container */}
         {/* pass in the loading and users as props through state */}
-        <Users loading={loading} users={users} />
+        <Alert alert={this.state.alert} />
+        <Switch>
+          <Route exact path='/' render={props => (
+            <Fragment>
+              <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} 
+                //if there are any users being shown, showClear is true, otherwise it is false
+                showClear={users.length > 0 ? true : false }
+                setAlert={this.setAlert}/>
+                <Users loading={loading} users={users} />
+            </Fragment>
+          )}></Route>
+          <Route exact path='/about' component={About}></Route>
+        </Switch>
         </div>  
       </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ icon, title }) => {
 //Do not need to run the render() command if it is a function and not a state
         return (
+          <nav className='navbar bg-primary'>
             <div>
                 <h1>
                     <i className={icon} />{title}
                 </h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/About">About</Link>
+                    </li>
+                </ul>
             </div>
+            </nav>  
         )
 }
 
